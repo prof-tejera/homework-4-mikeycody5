6849d49750abe52308c86f582bfb4630424bb5e1
mikeycody5 <mikeycody5@gmail.com>
2023-10-12 18:43:43 -0400

    calculator complete

--- src/components/Number.js
+++ src/components/Number.js
@@ -1,13 +1,6 @@
 const Number = ({ value, onClick }) => {
-  /** TODO: What happens when a user clicks a number, what do we want to pass to our parent? */
   return (
-    <div
-      style={{
-        padding: 10,
-        border: "1px solid black",
-        width: 60,
-      }}
-    >
+    <div className="Number" onClick={() => onClick(value)}>
       {value}
     </div>
   );
--- src/components/Operation.js
+++ src/components/Operation.js
@@ -1,16 +1,10 @@
-const Operation = ({ value }) => {
-  /** TODO: What happens when a user clicks an Operation, what do we want to pass to our parent? */
+const Operation = ({ value, onClick }) => {
   return (
-    <div
-      style={{
-        padding: 10,
-        border: "1px solid black",
-        width: 60,
-      }}
-    >
+    <div className="Operation" onClick={() => onClick(value)}>
       {value}
     </div>
   );
 };
+
 
 export default Operation;
